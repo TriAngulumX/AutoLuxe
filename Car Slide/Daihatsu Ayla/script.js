3976e23e2f4f07ed5cf0a176a573b834871cf92c
Johnstri <johanstri666@gmail.com>
2024-12-06 04:25:23 +0700

    Add files via upload

---
+++ Car Slide/Daihatsu Ayla/script.js
@@ -0,0 +1,101 @@
+/// h3 animation start
+const h3 = document.querySelectorAll(".hero .content h3");
+const obss = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("h3--visible");
+      }
+    });
+  },
+  {
+    root: null, // Mengamati dari viewport utama
+    rootMargin: "0px", // Tanpa margin tambahan
+    threshold: 0.1, // Elemen minimal 10% terlihat untuk mengaktifkan animasi
+  }
+);
+// Pasangkan observer ke setiap elemen dalam daftar
+h3.forEach((element) => obss.observe(element));
+/// h3 animation end
+
+/// h4 animation start
+const h4 = document.querySelectorAll(".hero .content h4");
+const obs = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("h4--visible");
+      }
+    });
+  },
+  {
+    root: null, // Mengamati dari viewport utama
+    rootMargin: "0px", // Tanpa margin tambahan
+    threshold: 0.1, // Elemen minimal 10% terlihat untuk mengaktifkan animasi
+  }
+);
+h4.forEach((element) => obs.observe(element));
+/// h4 animation end
+
+///speedtext animation start
+const speed = document.querySelectorAll(".hero .speedtext");
+const obs10 = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("speedtext--visible");
+      }
+    });
+  },
+  {
+    root: null, // Mengamati dari viewport utama
+    rootMargin: "0px", // Tanpa margin tambahan
+    threshold: 0.1, // Elemen minimal 10% terlihat untuk mengaktifkan animasi
+  }
+);
+// Pasangkan observer ke setiap elemen dalam daftar
+speed.forEach((element) => obs10.observe(element));
+///speedtext animation end
+
+///engine animation start
+const engi = document.querySelectorAll(".hero .engine");
+const obs11 = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("engine--visible");
+      }
+    });
+  },
+  {
+    root: null, // Mengamati dari viewport utama
+    rootMargin: "0px", // Tanpa margin tambahan
+    threshold: 0.1, // Elemen minimal 10% terlihat untuk mengaktifkan animasi
+  }
+);
+// Pasangkan observer ke setiap elemen dalam daftar
+engi.forEach((element) => obs11.observe(element));
+///speedtext animation end
+
+const bttn = document.getElementById("button");
+
+document
+  .getElementById("email-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    bttn.value = "Sending....";
+    const serviceid = "service_45bmfx9";
+    const templateid = "template_wloxvp9";
+
+    emailjs.sendForm(serviceid, templateid, this).then(
+      () => {
+        bttn.value = "Send Email";
+        alert("Email Sent.");
+      },
+      (err) => {
+        bttn.value = "Send Email";
+        alert(JSON.stringify(err));
+      }
+    );
+  });
